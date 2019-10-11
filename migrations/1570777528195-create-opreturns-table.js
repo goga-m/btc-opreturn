845e4f99230c346295e138ad56ed9143dca5efe1
goga-m <goga.mamoulasvili@gmail.com>
2019-10-11 20:29:09 +0300

    Add npm scripts
    * npm run migration - Create tables on initialization
    * npm run indexing - Start indexing & monitoring process
    * npm run server - Run server

--- migrations/1570777528195-create-opreturns-table.js
+++ migrations/1570777528195-create-opreturns-table.js
@@ -1,8 +1,8 @@
 // 'use strict'
-const db = require('../db')
+const { db } = require('../src/db')
 
 module.exports.up = function (next) {
-  db.query('CREATE TABLE IF NOT EXISTS op_returns (op_return text NOT NULL, txhash text NOT NULL, blockhash text NOT NULL)')
+  db.query('CREATE TABLE IF NOT EXISTS op_returns (op_return bytea NOT NULL, txhash text NOT NULL, blockhash text NOT NULL, blockheight integer NOT NULL)')
   .then(() => next())
   .catch(console.log)
 }
