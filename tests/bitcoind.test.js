7aca36525143290ff5e794b6528116b1394e38d3
goga-m <goga.mamoulasvili@gmail.com>
2019-10-14 11:48:33 +0300

    Add jest for tests

---
+++ tests/bitcoind.test.js
@@ -0,0 +1,20 @@
+const btcd = require('../src/rpc')()
+const { rpc } = require('../config')
+
+describe('Connection to bitcoind', () => {
+  // The assertion for a promise must be returned.
+  test(`Should connect to local node: ${rpc.protocol}://${rpc.host}:${rpc.port}`, () => {
+    return btcd('getblockchaininfo')
+    .then(data => {
+      expect(data).toBeDefined()
+      expect(data).toHaveProperty('headers')
+    })
+  })
+
+  test('bitcoind has finished IBD', () => {
+    return btcd('getblockchaininfo')
+    .then(data => {
+      expect(data.blocks).toEqual(data.headers)
+    })
+  })
+})
