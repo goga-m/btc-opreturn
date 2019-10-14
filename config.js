2bc0dd531c90c0aee6efc671855506962f5ce58a
goga-m <goga.mamoulasvili@gmail.com>
2019-10-14 11:47:23 +0300

    Add comments in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// Use rpc config as a Node module in case env variables are needed  (dotenv)
 module.exports = {
+  // Database settings
   postgresql: {
     user: 'postgres',
     host: 'localhost',
@@ -7,17 +7,20 @@ module.exports = {
     password: 'psql',
     port: 5432
   },
+  // JSON-RPC settings (bitcoind)
   rpc: {
     protocol: 'http', // Optional. Will be http by default
     host: '127.0.0.1', // Will be 127.0.0.1 by default
     user: 'rpcuser', // Optional, only if auth needed
-    password: 'rpcpassword', // Optional. Can be named 'pass'. Mandatory if user is passed.
+    password: 'rpcpassword', // Optional. Mandatory if user is passed.
     port: 8339
   },
+  // Indexing settings
   index: {
     startingBlockHeight: 599068
   },
+  // Endpoint server settings
   server: {
-    port: 3000
+    port: 3000 // https://localhost:3000/opreturn/${opReturnData}
   }
 }
