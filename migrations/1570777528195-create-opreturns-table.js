7ed7c492ba129351e001f156036e162883be94c3
goga-m <goga.mamoulasvili@gmail.com>
2019-10-14 11:32:48 +0300

    Db operations
    * op_return column is bytea hex
    * Format op_record column on write/read db operations

--- migrations/1570777528195-create-opreturns-table.js
+++ migrations/1570777528195-create-opreturns-table.js
@@ -4,7 +4,7 @@ const log = require('../src/logger')
 
 module.exports.up = function (next) {
   log.info('Creating op_returns table')
-  db.query('CREATE TABLE IF NOT EXISTS op_returns (op_return text NOT NULL, txhash text NOT NULL, blockhash text NOT NULL, blockheight integer NOT NULL)')
+  db.query('CREATE TABLE IF NOT EXISTS op_returns (op_return bytea NOT NULL, txhash text NOT NULL, blockhash text NOT NULL, blockheight integer NOT NULL, unique(op_return, txhash))')
   .then(() => next())
   .catch(err => {
     console.log('Error creating op_returns table')
