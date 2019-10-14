6b21403749c516147aebb5f83c7fbba133954d11
goga-m <goga.mamoulasvili@gmail.com>
2019-10-14 11:43:39 +0300

    New npm command: npm run index <startBlockHeight> <endBlockHeight>

--- bin/start-indexing.js
+++ bin/start-indexing.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 const OPIndex = require('../src/opindex')
+const log = require('../src/logger')
+
+const startBlockHeight = process.argv[2]
+const endBlockHeight = process.argv[3]
 
 const idx = OPIndex()
-idx.monitor()
+idx.indexBlocks(startBlockHeight, endBlockHeight)
+.catch(e => {
+  log.error(e)
+})
