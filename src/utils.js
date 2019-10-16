36ff9955b91b1c5517c1e3f5856c6fe0cd4a3cd9
goga-m <goga.mamoulasvili@gmail.com>
2019-10-16 07:25:24 +0300

    Handle errors at the block level

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,14 @@
 const bitcoinjs = require('bitcoinjs-lib')
-const { mapSeries, timesSeries } = require('async')
+const { mapSeries } = require('async')
 const _ = require('lodash/fp')
 
+const config = require('../config')
 const btc = require('./rpc')
-const { saveOpMeta, getIndexedBlockHeight } = require('./db')
+const { saveOpMeta } = require('./db')
 const { extractOpData } = require('./op_utils')
 const log = require('./logger')
+
+const IDLE_BETWEEN_TXS = _.get('index.idleBetweenTxs', config) || 1
 
 /**
  * Parses the output scripts, 
@@ -22,10 +25,12 @@ const log = require('./logger')
  */
 const extractOpMeta = tx => {
   const scriptPubKeys = _.map('scriptPubKey', tx.vout)
+
   const meta = _.map(out => {
     const script = Buffer.from(out.hex, 'hex')
     return extractOpData(script)
   }, scriptPubKeys)
+
   return _.compact(meta)
 }
 
@@ -41,13 +46,56 @@ const extractOpMeta = tx => {
  * @returns {Promise}
  */
 const saveTxOpMeta = (tx, blockHash, blockHeight) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const meta = extractOpMeta(tx)
     mapSeries(meta, (opcode, next) => {
       saveOpMeta(opcode, tx.txid, blockHash, blockHeight)
       .then(saved => next(null, saved))
     }, (err, all) =>{
       resolve(all)
+    })
+  })
+}
+
+/**
+ * Iterate of a list of transactions, 
+ * extract and save all OP_RETURN Metatags
+ *
+ * @name indexTxs
+ * @function
+ * @param {Array<Object>} txs Transactions array
+ * @param {String} blockHash
+ * @param {String} blockHeight
+ * @returns {Promise<Object>} { totalIndexed }
+ */
+const indexTxs = (txs, blockHash, blockHeight) => {
+  return new Promise((resolve, reject) => {
+    // Parse txs array sequentially 
+    mapSeries(txs, (tx, next) => {
+      // Extract and save all metatags for 
+      // this transaction (if found)
+      saveTxOpMeta(tx, blockHash, blockHeight)
+      .then(savedOps => {
+        // Log all savedOps saved for a transaction
+        _.forEach(op => {
+          log.info('Indexing', 'BLOCK:', blockHeight, 'TXHASH:', op.txhash.substring(0, 8), 'OP_RETURN:', op.op_return)
+        }, savedOps)
+
+        // Success
+        setTimeout(() => {
+          next(null, savedOps.length)
+        }, savedOps.length * IDLE_BETWEEN_TXS)
+      })
+      .catch(err => {
+        log.error('Failed indexing tx:', tx.txid)
+        next(err)
+      })
+
+    }, (err, all) => {
+      if(err) return reject(err)
+      // Resolve
+      const totalIndexed = _.sum(_.compact(all))
+      resolve({ success: !err, totalIndexed })
     })
   })
 }
@@ -68,38 +116,16 @@ const saveTxOpMeta = (tx, blockHash, blockHeight) => {
  * @returns {Promise<Object>} Return the total meta indexed and the errored
  */
 const indexBlock = blockHeight => {
-  return new Promise((resolve, reject) => {
-    // Errored metadata from operations
-    const errored = []
-    btc('getblockhash', [blockHeight])
-    .then(hash => btc('getblock', [hash, 2]))
-    .then(block => {
-      // Index and Store each transaction's sequentially
-      mapSeries(block.tx, (tx, next) => {
-        // Save all metatags of this transaction (if found)
-        saveTxOpMeta(tx, block.hash, blockHeight)
-        .then(opreturns => {
-          // Log all opreturns saved for a transaction
-          _.forEach(op => {
-            log.info('Indexing', 'BLOCK:', blockHeight, 'TXHASH:', op.txhash.substring(0, 8), 'OP_RETURN:', op.op_return)
-          }, opreturns)
-          // Success
-          next(null, opreturns.length)
-        })
-        .catch(err => {
-          log.error('Indexing ', err)
-          // TODO: handle
-        })
-
-      }, (err, all) => {
-        // all is an array tha contains the total metadata entries saved per transaction
-        const totalIndexed = _.sum(_.compact(all))
-        // Returned totalIndexed and errored ones
-        log.info(`Finished ${blockHeight} Indexed ${totalIndexed} OP_RETURN records.`)
-        resolve({ totalIndexed, errored })
-      })
-    })
-    .catch(reject)
+  return btc('getblockhash', [blockHeight])
+  .then(hash => btc('getblock', [hash, 2]))
+  .then(block => indexTxs(block.tx, block.hash, blockHeight))
+  .then(({ totalIndexed }) => {
+    log.info(`Finished ${blockHeight} Indexed ${totalIndexed} OP_RETURN records.`)
+    return { totalIndexed }
+  })
+  .catch(err => {
+    log.error(`Failed to index all OP_RETURN metadata for ${blockHeight}.`, err)
+    throw err
   })
 }
 
