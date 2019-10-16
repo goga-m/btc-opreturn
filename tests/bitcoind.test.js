fbea0e85135d68cdcd15742ed9bcb865241ef96f
goga-m <goga.mamoulasvili@gmail.com>
2019-10-16 14:51:46 +0300

    Tests

--- tests/bitcoind.test.js
+++ tests/bitcoind.test.js
@@ -4,17 +4,21 @@ const { rpc } = require('../config')
 describe('Connection to bitcoind', () => {
   // The assertion for a promise must be returned.
   test(`Should connect to local node: ${rpc.protocol}://${rpc.host}:${rpc.port}`, () => {
+    expect.assertions(2)
     return btcd('getblockchaininfo')
     .then(data => {
       expect(data).toBeDefined()
       expect(data).toHaveProperty('headers')
+      return true
     })
   })
 
   test('bitcoind has finished IBD', () => {
+    expect.assertions(1)
     return btcd('getblockchaininfo')
     .then(data => {
       expect(data.blocks).toEqual(data.headers)
+      return true
     })
   })
 })
