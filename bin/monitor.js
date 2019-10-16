35912cba99adee280f940dad38d7569bdcb52cc5
goga-m <goga.mamoulasvili@gmail.com>
2019-10-16 15:32:16 +0300

    Check command block height inputs

--- bin/monitor.js
+++ bin/monitor.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const check = require('check-types')
 const OPIndex = require('../src/opindex')
 const log = require('../src/logger')
 
@@ -6,6 +7,11 @@ const startBlockHeight = process.argv[2]
 
 const idx = OPIndex()
 const start = parseInt(startBlockHeight)
+
+if (check.assigned(startBlockHeight) && !check.integer(start)) {
+  log.error('Starting block should be an integer')
+  return
+}
 
 log.info('Start monitoring...')
 idx.monitor(start)
